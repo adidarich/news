4aaca0186a15eefc4eee46d83270c0817fdbc5d9
adidarich <adidarich1994@gmail.com>
2023-05-25 14:13:18 +0500

    добавил отработанные детали

--- myblog/news/models.py
+++ myblog/news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -18,7 +19,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=37)
     slug = models.SlugField(
-        max_length=37)  # Слаг – это короткая метка, содержащая только буквы, цифры, знаки подчеркивания или дефисы.
+        max_length=37, unique_for_date='publish')  # Слаг – это короткая метка, содержащая только буквы, цифры, знаки подчеркивания или дефисы.
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='news_posts')
@@ -41,3 +42,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('news:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
--- myblog/news/views.py
+++ myblog/news/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list,
+                          2)  # создаем экземпляр класса Paginator с числом объектов, возвращаемых в расчете на страницу
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'news/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):  # детальная информация о посте
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):  # детальная информация о посте
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'news/post/detail.html', {'post': post})
